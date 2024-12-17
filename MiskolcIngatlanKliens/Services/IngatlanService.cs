982793131220e6898c4fbe6cb680fc550d829fdc
Dombai Zoltán <dombaiz@kkszki.hu>
2024-12-17 14:39:24 +0100

    Órai mentés

---
+++ MiskolcIngatlanKliens/Services/IngatlanService.cs
@@ -0,0 +1,38 @@
+﻿using MiskolciIngatlanKliens.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MiskolcIngatlanKliens.Services
+{
+    internal class IngatlanService
+    {
+        public async static Task<List<Ingatlan>> GetAll(HttpClient client)
+        {
+            List<Ingatlan> result = await client.GetFromJsonAsync<List<Ingatlan>>("Ingatlan");
+            if (result is not null)
+            {
+                return result;
+            }
+            else
+            {
+                return new List<Ingatlan>();
+            }
+        }
+
+        public async static Task<string> InsterNew(HttpClient client, Ingatlan ujIngatlan)
+        {
+            string uj = JsonSerializer.Serialize(ujIngatlan, JsonSerializerOptions.Default);
+            string url = $"{client.BaseAddress}Ingatlan";
+            var request = new StringContent(uj, Encoding.UTF8 , "application/json") ;
+            var response = await client.PostAsync(url, request);
+            var valasz = await response.Content.ReadAsStringAsync();
+            return valasz;
+        }
+    }
+}
--- MiskolcIngatlanKliens/SubWindows/IngatlanokWindow.xaml.cs
+++ MiskolcIngatlanKliens/SubWindows/IngatlanokWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using MiskolciIngatlanKliens.Models;
+using MiskolcIngatlanKliens.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -19,24 +22,50 @@ namespace MiskolcIngatlanKliens.SubWindows
     /// </summary>
     public partial class IngatlanokWindow : Window
     {
+        private HttpClient sajatKliens = new HttpClient()
+        {
+            BaseAddress = new Uri("http://localhost:5000")
+        };
+        private List<Ingatlan> ingatlanok = new List<Ingatlan>();
         public IngatlanokWindow()
         {
             InitializeComponent();
+            ingatlanok = IngatlanService.GetAll(sajatKliens).Result;
+            dgrIngatlanok.ItemsSource = ingatlanok;
+            Feltolt();
         }
 
-        private void btnUj_Click(object sender, RoutedEventArgs e)
+        private async void BtnUj_Click(object sender, RoutedEventArgs e)
+        {
+            Ingatlan ujIngatlan = new Ingatlan()
+            {
+                Telepules = tbxTelepules.Text,
+                Cim = tbxCim.Text,
+                Ar = int.Parse(tbxAr.Text),
+                Tipus = tbxTipus.Text,
+                KepNev = tbxKepNev.Text,
+                UgyintezoId = 1,
+                Ugyintezo = null
+            };
+            string uzenet = await IngatlanService.InsterNew(sajatKliens, ujIngatlan);
+            MessageBox.Show(uzenet);
+        }
+
+        private void BtnModositas_Click(object sender, RoutedEventArgs e)
         {
 
         }
 
-        private void btnModositas_Click(object sender, RoutedEventArgs e)
+        private void BtnTorol_Click(object sender, RoutedEventArgs e)
         {
 
         }
 
-        private void btnTorol_Click(object sender, RoutedEventArgs e)
+        private async void Feltolt()
         {
-
+            ingatlanok = await IngatlanService.GetAll(sajatKliens);
+            Task.Delay(1000).Wait();
+            dgrIngatlanok.ItemsSource = ingatlanok;
         }
     }
 }
